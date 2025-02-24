57cee4fea355628976bc466b007352e3868b6420
yawatamikiya <yawatamikiya>
2025-02-25 00:33:44 +0900

    使用例更新

--- sample_app/python/IpatHelper_PythonTest.py
+++ sample_app/python/IpatHelper_PythonTest.py
@@ -11,7 +11,6 @@ def main():
         # ログイン処理(各自自分のIDに変えてください)
         returnValue = login('********', '********', '****', '****')
         if (returnValue & 1) != 1:
-            uninit()
             print("ログインに失敗しました。")
             return
 
@@ -20,8 +19,6 @@ def main():
         returnValue = get_bet_instance(KAISAI_NAKAYAMA, 11, 2020, 12, 27,
                         HOUSHIKI_FORMATION, SHIKIBETSU_TRIO, 100, "1,9-2,3,13-7,3,8,10", betData)
         if (returnValue & 1) != 1:
-            logout()
-            uninit()
             print("馬券購入情報の取得に失敗しました。")
             return
 
@@ -30,8 +27,6 @@ def main():
         betDataList[0] = betData
         returnValue = bet(betDataList, 1, 0)
         if (returnValue & 1) != 1:
-            logout()
-            uninit()
             print("馬券購入に失敗しました。")
             return
 
@@ -39,16 +34,12 @@ def main():
         betDataWin5 = ST_BET_DATA_WIN5()
         returnValue = get_bet_instance_win5(100, 2020, 12, 27, "1,14-9,13-12-2-1,1,3,5", betDataWin5)
         if (returnValue & 1) != 1:
-            logout()
-            uninit()
             print("馬券購入情報(WIN5)の取得に失敗しました。")
             return
 
         # 馬券購入処理実行(WIN5)
         returnValue = bet_win5(betDataWin5, 0)
         if (returnValue & 1) != 1:
-            logout()
-            uninit()
             print("馬券購入(WIN5)に失敗しました。")
             return
     
